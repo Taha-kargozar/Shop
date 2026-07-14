0f7a56ee73b752ab486a1497ff8ab6ca4b58a51d
taha <taha1060@yahoo.com>
2026-07-14 19:46:53 +0330

    Add JWT

---
+++ src/main/java/ir/shop/shop/jwt/JwtAuthFilter.java
@@ -0,0 +1,58 @@
+package ir.shop.shop.jwt;
+
+import ir.shop.shop.service.CustomUserDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+    private final CustomUserDetailsService userDetailsService;
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request,response);
+            return;
+        }
+        String token = authHeader.substring(7);
+        String email = jwtService.extractEmail(token);
+        if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+            if (jwtService.tokenValidate(token, userDetails)) {
+
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(
+                                userDetails,
+                                null,
+                                userDetails.getAuthorities()
+                        );
+
+                authentication.setDetails(
+                        new WebAuthenticationDetailsSource()
+                                .buildDetails(request)
+                );
+
+                SecurityContextHolder.getContext()
+                        .setAuthentication(authentication);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
+
---
+++ src/main/java/ir/shop/shop/jwt/JwtService.java
@@ -0,0 +1,65 @@
+package ir.shop.shop.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import ir.shop.shop.model.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.jpa.repository.query.KeysetScrollDelegate;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class JwtService {
+    @Value("${jwt.secret}")
+    private String sec;
+
+    @Value("${jwt.expire}")
+    private long exp;
+
+    private SecretKey getKeySign() {
+        return Keys.hmacShaKeyFor(sec.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String tokenGenerate(User user) {
+        return Jwts.builder().subject(user.getEmail()).claim(
+                "role",
+                        user.getRole().getName()
+        ) .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + exp))
+                .signWith(getKeySign())
+                .compact();
+    }
+    private Claims parseClaims(String token) {
+        return Jwts.parser().verifyWith(getKeySign()) .build().parseSignedClaims(token).getPayload();
+    }
+
+    public String extractEmail (String token) {
+        return parseClaims(token).getSubject();
+    }
+
+    public String extractRole (String token) {
+        return parseClaims(token).get("role",String.class);
+    }
+    public List<SimpleGrantedAuthority> extractAuth (String token) {
+        String role = extractRole(token);
+
+        if(role == null) {
+            return List.of();
+        }
+
+        return List.of(new SimpleGrantedAuthority(role));
+    }
+    public boolean isTokenExp(String token) {
+        return parseClaims(token).getExpiration().before(new Date());
+    }
+    public boolean tokenValidate(String token, UserDetails userDetails) {
+        return extractEmail(token).equals(userDetails.getUsername()) && !isTokenExp(token);
+    }
+}
